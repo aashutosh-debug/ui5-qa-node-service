121d1fad0311b644333f12928a6a65c36d263fe7
aashutosh-debug <rambhalashutosh@gmail.com>
2025-09-04 15:43:37 +0530

    Api Routes created

--- index.js
+++ index.js
@@ -13,17 +13,18 @@ const PORT = 3000;
 
 const { Pool } = pg;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
+//Company
 // Auth: Signup
-app.post("/auth/signup", async (req, res) => {
+app.post("/auth/company/signup", async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, website, phone, company } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const result = await pool.query(
-      "INSERT INTO companies (name, email, password) VALUES ($1, $2, $3) RETURNING id",
-      [name, email, hashedPassword]
+      "INSERT INTO companies (name, email, password, website, phone, company ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
+      [name, email, hashedPassword, website, phone, company]
     );
     res.json({ success: true, user: result.rows[0] });
   } catch (err) {
@@ -31,15 +32,177 @@ app.post("/auth/signup", async (req, res) => {
   }
 });
 
-app.get('/', (req, res)=>{
-    res.status(200);
-    res.send("Welcome to root URL of Server");
+// Auth: Login
+app.post("/auth/company/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const result = await pool.query("SELECT * FROM companies WHERE email=$1", [
+      email,
+    ]);
+    if (result.rows.length === 0)
+      return res.status(401).json({ error: "User not found" });
+
+    const user = result.rows[0];
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(401).json({ error: "Invalid password" });
+
+    const token = "token"; //generateToken(user);
+    res.json({ success: true, token, role: user.role });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT);
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+//Jobs
+app.post("/addjobs", async (req, res) => {
+  try {
+    const { title, description, company_id } = req.body;
+    const result = await pool.query(
+      "INSERT INTO jobs (title, description, company_id) VALUES ($1, $2, $3) RETURNING id",
+      [title, description, company_id]
+    );
+    res.json({ success: true, jobs: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//Questions
+app.post("/question", async (req, res) => {
+  try {
+    const {
+      job_id,
+      question_text,
+      question_type,
+      company_id,
+      difficulty,
+      created_by,
+      tags,
+    } = req.body;
+    const result = await pool.query(
+      "INSERT INTO questions (job_id, question_text, question_type, company_id, difficulty, created_by, tags ) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+      [
+        job_id,
+        question_text,
+        question_type,
+        company_id,
+        difficulty,
+        created_by,
+        tags,
+      ]
+    );
+    res.json({ success: true, question: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/question/:job_id", async (req, res) => {
+  try {
+    const job_id = req.params.job_id;
+    const result = await pool.query("SELECT * FROM questions WHERE job_id=$1", [
+      job_id,
+    ]);
+    if (result.rows.length === 0)
+      return res.status(401).json({ error: "Job not found" });
+
+    res.json({ success: true, value: result.rows });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//Candidates
+app.post("/candidate", async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      phone,
+      skills,
+      experience,
+      location 
+    } = req.body;
+    const result = await pool.query(
+      "INSERT INTO candidates (name, email, password, phone, skills, experience, location) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+      [
+        name,
+        email,
+        password,
+        phone,
+        skills,
+        experience,
+        location 
+      ]
+    );
+    res.json({ success: true, test: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//Tests
+app.post("/test", async (req, res) => {
+  try {
+    const {
+      job_post_id,
+      candidate_id
+    } = req.body;
+    const result = await pool.query(
+      "INSERT INTO tests (job_post_id, candidate_id, status) VALUES ($1, $2, $3) RETURNING id",
+      [
+        job_post_id,
+        candidate_id,
+        "CREATED"
+      ]
+    );
+    res.json({ success: true, test: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/test/start/:id", async (req, res) => {
+  try {
+    const id =  req.params.id;
+    const result = await pool.query(
+      "UPDATE tests SET start_time = NOW(), status = 'STARTED' WHERE id = $1 RETURNING id",
+      [
+        id
+      ]
+    );
+    res.json({ success: true, value: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/test/end/:id", async (req, res) => {
+  try {
+    const id =  req.params.id;
+    const result = await pool.query(
+      "UPDATE tests SET end_time = NOW(), status = 'ENDED' WHERE id = $1 RETURNING id",
+      [
+        id
+      ]
+    );
+    res.json({ success: true, value: result.rows[0] });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// app.get('/', (req, res)=>{
+//     res.status(200);
+//     res.send("Welcome to root URL of Server");
+// });
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, and App is listening on port " + PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
